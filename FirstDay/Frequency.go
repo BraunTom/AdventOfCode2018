279f3e44c6fdb9b53c5dcd741013050023580452
Tom Braun <brauntom2@t-online.de>
2018-12-29 23:40:32 +0100

    clears first day

---
+++ FirstDay/Frequency.go
@@ -0,0 +1,52 @@
+package FirstDay
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func fileData() []string {
+	input, _ := ioutil.ReadFile("FirstDay/input.txt")
+	return strings.Split(string(input), "\n")
+}
+
+func Frequency() int {
+	sum := 0
+
+	for _, v := range fileData() {
+		number, _ := strconv.Atoi(v)
+		sum += number
+		fmt.Println(sum)
+	}
+
+	return sum
+}
+
+func Twice() int {
+	set := make(map[int]bool)
+	data := fileData()
+	sum := 0
+
+	set[0] = true
+
+	for {
+		for _, v := range data {
+			number, err := strconv.Atoi(v)
+
+			if err != nil {
+				continue
+			}
+
+			sum += number
+
+			fmt.Println(sum)
+			if set[sum] {
+				return sum
+			}
+
+			set[sum] = true
+		}
+	}
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"AdventOfCode2018/FirstDay"
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println(FirstDay.Twice())
+}
